f4259f7303d1ac8a32e6e09f5f660f6e9d53bd21
livmeierx <liv.meier13@gmail.com>
2022-05-23 17:27:58 -0400

    initial setup

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
 // Assignment code here
+//function to ask users password criteria
+var passCriteria = function (){
+  var specialChar = prompt("Do you want special characters included?");
 
+  if (specialChar = "yes"
+
+
+
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -8,6 +17,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
