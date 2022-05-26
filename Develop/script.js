b3023f1336145b1d19b172e6d0e681d91e364afd
livmeierx <liv.meier13@gmail.com>
2022-05-25 22:17:34 -0400

    trying to connect generator with button

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-var generateBtn = document.querySelector("generate");
+var generateBtn = document.querySelector("#generate");
 
 var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -19,50 +19,54 @@ window.alert("Welcome to Liv's Password Generator!")
 // };
 
 //function to generate password and user prompts
-function generatePassword() {
-  var password = "";
-  var randomPass = "";
+function passQuestions() {
+  var result = "";
 
   var passwordLength = prompt("Please enter a password length between 8 and 128 characters.");
   passwordLength = parseInt(passwordLength);
 
-  var lowercaseOption = alert("Do you want to include lowercase?");
+  var lowercaseOption = confirm("Do you want to include lowercase?");
   if (lowercaseOption) {
-    randomPass += lowercase;
+    result += lowercase;
   }
 
-  var uppercaseOption = alert("Do you want to include UPPERCASE?");
+  var uppercaseOption = confirm("Do you want to include UPPERCASE?");
   if (uppercaseOption) {
-    randomPass += uppercase;
+    result += uppercase;
   }
 
-  var numericalOption = alert("Do you want to include numbers?");
+  var numericalOption = confirm("Do you want to include numbers?");
   if (numericalOption) {
-    randomPass += numerical;
+    result += numerical;
   }
 
-  var specialOption = alert("Do you want to include special characters?");
+  var specialOption = confirm("Do you want to include special characters?");
   if (specialOption) {
-    ranadomPass += special;
+    result += special;
   }
+};
 
+// Get references to the #generate element
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var password = "";
   for (var i = 0; i < passwordLength; i++){
-    password = randomPass[Math.floor(Math.random() * randomPass.length)];
-  }
+    password = result[Math.floor(Math.random() * result.length)];
 
+  return password;
+  }
+  generatePassword();
 }; 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("password");
-
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
-// Get references to the #generate element
-generateBtn.addEventListener("click", generatePassword);
 
-generatePassword();
+passQuestions();
 writePassword();
 
